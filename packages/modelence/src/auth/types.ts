e2b8649bbf13fc87f63b470329a5996c1bacf265
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2026-02-27 21:42:09 +0530

    refactor(auth): modularize OAuth flow and add typed User model

--- packages/modelence/src/auth/types.ts
+++ packages/modelence/src/auth/types.ts
@@ -1,7 +1,38 @@
 import { Document, ObjectId } from 'mongodb';
 import { ConnectionInfo } from '@/methods/types';
 
-export type User = Document;
+export interface User extends Document {
+  _id: ObjectId;
+
+  handle: string;
+
+  emails?: {
+    address: string;
+    verified: boolean;
+  }[];
+
+  status?: 'active' | 'disabled' | 'deleted';
+
+  createdAt: Date;
+  disabledAt?: Date;
+  deletedAt?: Date;
+
+  roles?: string[];
+
+  authMethods: {
+    password?: {
+      hash: string;
+    };
+    google?: {
+      id: string;
+    };
+    github?: {
+      id: string;
+    };
+  };
+}
+
+// export type User = Document;
 
 export type UserInfo = {
   id: string;
